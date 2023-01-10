abe21589edffb2209e4b4c639dbaa520350f5d42
Taj <sharma.tajinder01@gmail.com>
2023-01-09 16:19:45 -0800

    Add loop to print error if number is less than 0, otherwise the loop executes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,number) {
+    let word = ''
+    if(number<0){
+        return 'ERROR';
+    }else{
+        for(let i =0; i<number;i++){
+            word += string;
+        }
+    }
+    return word
 };
 
 // Do not edit below this line
